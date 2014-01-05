726aa4af81ee415f7ca588cac3661efa7e667963
c9s <cornelius.howl@gmail.com>
2014-01-06 04:25:23 +0800

    Add compiled hello_mux.php

---
+++ eg/hello_mux.php
@@ -0,0 +1,23 @@
+<?php return Phux\Mux::__set_state(array(
+   'routes' => 
+  array (
+    0 => 
+    array (
+      0 => false,
+      1 => '/hello',
+      2 => 
+      array (
+        0 => 'HelloController',
+        1 => 'helloAction',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+   'subMux' => 
+  array (
+  ),
+   'id' => NULL,
+   'expandSubMux' => true,
+)); /* version */
