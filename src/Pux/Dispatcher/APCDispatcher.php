a8d6615a4ff8cded7f6ce378604cf34e91675e66
c9s <cornelius.howl@gmail.com>
2014-01-14 01:28:07 +0800

    Add APCDispatcher class.

---
+++ src/Pux/Dispatcher/APCDispatcher.php
@@ -0,0 +1,50 @@
+<?php
+namespace Pux;
+use Pux\Mux;
+
+class APCDispatcher
+{
+    public $options = array();
+
+    public $mux;
+
+    public $namespace = 'app';
+
+    public $expiry = 0;
+
+    public function __construct(Mux $mux, $options = array())
+    {
+        $this->mux = $mux;
+        $this->options = $options;
+        if ( isset($this->options['namespace']) ) {
+            $this->namespace = $this->options['namespace'];
+        }
+        if ( isset($this->options['expiry']) ) {
+            $this->expiry = $this->options['expiry'];
+        }
+    }
+
+    public function getNamespace() {
+        return $this->namespace;
+    }
+
+
+    public function getExpiry()
+    {
+        return $this->expiry;
+    }
+
+    public function dispatch($path) 
+    {
+        $key = $this->getNamespace() . ':' . $path;
+        if ( $route = apc_fetch($key) ) {
+            return $route;
+        }
+        $route = $mux->dispatch($path);
+        apc_store($key, $route, $this->expiry);
+        return $route;
+    }
+}
+
+
+
