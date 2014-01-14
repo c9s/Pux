8c6d24fb2d438926b63cc98b39a3fc43d0dbf4e6
c9s <cornelius.howl@gmail.com>
2014-01-14 18:55:43 +0800

    travis: remove 5.5 condition

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -11,10 +11,10 @@ fi
 if [[ $(phpenv version-name) =~ 5.[34] ]] ; then
     echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
-if [[ $(phpenv version-name) =~ 5.5 ]] ; then
-    # echo yes | pecl install -f  APCu-beta
-    # echo "extension=apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-fi
+# if [[ $(phpenv version-name) =~ 5.5 ]] ; then
+#     # echo yes | pecl install -f  APCu-beta
+#     # echo "extension=apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+# fi
 if [[ $(phpenv version-name) =~ hhvm ]] ; then
     sudo apt-get install php5-dev
 fi
