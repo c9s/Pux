4481a294b7a5e0c5a16d4146a25bb687dd2f4d79
c9s <cornelius.howl@gmail.com>
2014-01-14 18:50:56 +0800

    apcu is already loaded in 5.5

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -12,8 +12,8 @@ if [[ $(phpenv version-name) =~ 5.[34] ]] ; then
     echo "extension=apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
 if [[ $(phpenv version-name) =~ 5.5 ]] ; then
-    echo yes | pecl install -f  APCu-beta
-    echo "extension=apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    # echo yes | pecl install -f  APCu-beta
+    # echo "extension=apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
 if [[ $(phpenv version-name) =~ hhvm ]] ; then
     sudo apt-get install php5-dev
