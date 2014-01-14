566683f6609280798637c6cf3d95cb156b56001c
c9s <cornelius.howl@gmail.com>
2014-01-14 17:51:42 +0800

    Add Extension test script for travis-ci

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+echo "Basic testing..."
 phpunit --debug
+
+echo "Stress testing..."
 phpunit --repeat 1000
+
+echo "Testing pux extension..."
+if [[ $(phpenv version-name) =~ "5.[345]" ]] ; then
+    cd ext
+    extunit --phpunit --debug MuxTest.php
+    extunit --phpunit --debug ControllerTest.php
+
+    echo "Stress testing..."
+    extunit --phpunit --repeat 1000 MuxTest.php
+    extunit --phpunit --repeat 1000 ControllerTest.php
+    cd ..
+fi
