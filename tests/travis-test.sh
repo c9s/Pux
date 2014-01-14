ecbfffca76d8da585535880e39884b96efabc9b3
c9s <cornelius.howl@gmail.com>
2014-01-14 19:23:34 +0800

    exit 1 if test failed

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -8,11 +8,11 @@ phpunit --repeat 100
 if [[ $(phpenv version-name) =~ 5.[345] ]] ; then
     echo "Testing pux extension..."
     cd ext
-    extunit --phpunit --verbose MuxTest.php
-    extunit --phpunit --verbose ControllerTest.php
+    extunit --phpunit --verbose MuxTest.php || exit 1
+    extunit --phpunit --verbose ControllerTest.php || exit 1
 
     echo "Stress testing on extension..."
-    extunit --phpunit --repeat 100 MuxTest.php
-    extunit --phpunit --repeat 100 ControllerTest.php
+    extunit --phpunit --repeat 100 MuxTest.php || exit 1
+    extunit --phpunit --repeat 100 ControllerTest.php || exit 1
     cd ..
 fi
