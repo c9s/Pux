67f68c13b39b14221c95736a1ea4c3d9f4f4911c
c9s <cornelius.howl@gmail.com>
2014-01-14 18:43:17 +0800

    apcc => apcu

--- tests/travis-setup.sh
+++ tests/travis-setup.sh
@@ -13,7 +13,7 @@ if [[ $(phpenv version-name) =~ "5.[34]" ]] ; then
 fi
 if [[ $(phpenv version-name) =~ "5.5" ]] ; then
     echo yes | pecl install -f  APCu-beta
-    echo "extension=apcc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    echo "extension=apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
 if [[ $(phpenv version-name) =~ "hhvm" ]] ; then
     sudo apt-get install php5-dev
