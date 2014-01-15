9b07e3106273a5823226051e71cca1e66628b431
c9s <cornelius.howl@gmail.com>
2014-01-15 16:31:03 +0800

    Checking in changes prior to tagging of version 1.3.2.

--- src/Pux/Console.php
+++ src/Pux/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpux';
-    const VERSION = "1.3.1";
+    const VERSION = "1.3.2";
 
     public function init()
     {
