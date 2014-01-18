e5f2e4013ad1c3c55cf75d3a522febf88767d62c
c9s <cornelius.howl@gmail.com>
2014-01-18 18:15:01 +0800

    update travis test script

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -11,9 +11,11 @@ if [[ $(phpenv version-name) =~ 5.[345] ]] ; then
     make clean && make || exit 1
     extunit --phpunit --debug MuxTest.php || exit 1
     extunit --phpunit --debug ControllerTest.php || exit 1
+    extunit --phpunit --debug PersistentMuxTest.php || exit 1
 
     echo "Stress testing on extension..."
     extunit --phpunit --repeat 100 MuxTest.php || exit 1
     extunit --phpunit --repeat 100 ControllerTest.php || exit 1
+    extunit --phpunit --repeat 100 PersistentMuxTest.php || exit 1
     cd ..
 fi
