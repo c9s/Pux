dd06bbd276bc1d2b596cf29abfb432ee1a1f2e39
c9s <cornelius.howl@gmail.com>
2014-01-20 02:15:00 +0800

    Add persistent dispatcher example code.

---
+++ eg/hello_persistent.php
@@ -0,0 +1,28 @@
+<?php
+// require '../src/Pux/Mux.php';
+// var_dump( $_SERVER ); 
+use Pux\Mux;
+class HelloController {
+    public function helloAction() {
+        return 'hello';
+    }
+}
+
+echo '<pre>';
+echo "process ", getmypid(), "\n";
+
+$r = pux_persistent_dispatch('hello', 'hello_mux.php', '/hello');
+echo '</pre>';
+var_dump( $r ); 
+/*
+ */
+    
+/*
+if ( $mux = pux_fetch_mux('hello') ) {
+    var_dump( $mux ); 
+} else {
+    $mux = require 'hello_mux.php';
+    pux_store_mux('hello', $mux);
+}
+ */
+// $r = $mux->dispatch('/hello');
