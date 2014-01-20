28619efb38e6ed3ecbe07898ae400bb87c18e4fb
c9s <cornelius.howl@gmail.com>
2014-01-20 15:34:11 +0800

    Update compile.sh, print less verbose messages

--- ext/compile.sh
+++ ext/compile.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-phpize --clean
-phpize && ./configure && make clean && make && make install
+echo "===> Cleaning..."
+phpize --clean > /dev/null
+echo "===> Scaffolding..."
+phpize || exit 1
+echo "===> Configuring..."
+./configure > /dev/null || exit 1
+echo "===> Cleaning..."
+make clean || exit 1
+echo "===> Building..."
+make clean > /dev/null && make > /dev/null && make install
