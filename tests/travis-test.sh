12a9e8911b2a2d58549cf7d5dcbcaa955888a937
c9s <cornelius.howl@gmail.com>
2014-01-28 20:08:32 +0800

    Add testsuite name

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Basic testing..."
-phpunit --debug || exit 1
+phpunit --testsuite PP --debug || exit 1
 
 echo "Stress testing..."
-phpunit --repeat 100 || exit 1
+phpunit --testsuite PP --repeat 100 || exit 1
 
 if [[ $(phpenv version-name) =~ 5.[345] ]] ; then
     echo "Testing pux extension..."
     cd ext
-    make clean && make || exit 1
+    ./compile -c || exit 1
     ./test -- --debug || exit 1
 
     echo "Stress testing on extension..."
