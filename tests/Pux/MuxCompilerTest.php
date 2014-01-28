217529d2fade96122801b55fdf7d2a66db774a8b
c9s <cornelius.howl@gmail.com>
2014-01-28 21:42:28 +0800

    fix test path

--- tests/Pux/MuxCompilerTest.php
+++ tests/Pux/MuxCompilerTest.php
@@ -14,24 +14,24 @@ class MuxCompilerTest extends PHPUnit_Framework_TestCase
     {
         $mux = new Mux;
         $mux->add('/hello/:name', [ 'FooController', 'index' ]);
-        $mux->compile("tests/hello_mux.php");
+        $mux->compile("hello_mux.php");
 
         $mux2 = new Mux;
         $mux2->add('/bye/:name', [ 'FooController', 'index' ]);
-        $mux2->compile("tests/bye_mux.php");
+        $mux2->compile("bye_mux.php");
 
         $compiler = new MuxCompiler;
-        ok( $compiler->load("tests/hello_mux.php") );
-        ok( $compiler->load("tests/bye_mux.php") );
+        ok( $compiler->load("hello_mux.php") );
+        ok( $compiler->load("bye_mux.php") );
 
         $compiler->compileReflectionParameters();
 
-        ok( $compiler->compile("tests/merged_mux.php") );
+        ok( $compiler->compile("merged_mux.php") );
 
 
-        path_ok( "tests/merged_mux.php" );
+        path_ok( "merged_mux.php" );
 
-        $mux = require "tests/merged_mux.php";
+        $mux = require "merged_mux.php";
         ok($mux);
 
         $routes = $mux->getRoutes();
@@ -43,9 +43,9 @@ class MuxCompilerTest extends PHPUnit_Framework_TestCase
         ok( $mux->dispatch('/hello/John') );
         ok( $mux->dispatch('/bye/John') );
 
-        unlink("tests/merged_mux.php");
-        unlink("tests/hello_mux.php");
-        unlink("tests/bye_mux.php");
+        unlink("merged_mux.php");
+        unlink("hello_mux.php");
+        unlink("bye_mux.php");
     }
 }
 
