7ec8486c5319272f80545244c41e2f3eceddd1ce
c9s <cornelius.howl@gmail.com>
2014-01-28 19:19:31 +0800

    put testing controllers to tests/controllers.php

---
+++ tests/controllers.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class HelloController
+{
+    public function index($name) {
+        return "Hello $name";
+    }
+
+    public function show() {
+        return "response";
+    }
+}
+
+
+class HelloController2
+{
+    public function helloAction($name) {
+        return "hello $name";
+    }
+}
+
+class ProductController
+{
+    public function indexAction() { return 'index'; }
+    public function fooAction() { return 'foo'; }
+    public function barAction() { return 'bar'; }
+
+    public function itemAction($id)  { 
+        return "product item $id";
+    }
+}
+
+class ExpandableProductController extends Pux\Controller
+{
+    public function indexAction() { }
+
+    public function itemAction() { }
+
+    public function addAction() { }
+
+    public function delAction() { }
+
+    public function fooBarAction() { }
+}
+
+
+
+class PageController {
+
+    public function page1() { return 'page1'; }
+    public function page2() { return 'page2'; }
+
+}
+
