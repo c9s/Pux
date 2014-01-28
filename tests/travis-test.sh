ace5b15808f10a63b3b622a0559623db7918127e
c9s <cornelius.howl@gmail.com>
2014-01-28 20:12:23 +0800

    always test extension

--- tests/travis-test.sh
+++ tests/travis-test.sh
@@ -5,13 +5,13 @@ phpunit --testsuite PP --debug || exit 1
 echo "Stress testing..."
 phpunit --testsuite PP --repeat 100 || exit 1
 
-if [[ $(phpenv version-name) =~ 5.[345] ]] ; then
-    echo "Testing pux extension..."
-    cd ext
-    ./compile -cp || exit 1
-    ./test -- --debug || exit 1
+# if [[ $(phpenv version-name) =~ 5.[345] ]] ; then
+echo "Testing pux extension..."
+cd ext
+./compile -cp || exit 1
+./test -- --debug || exit 1
 
-    echo "Stress testing on extension..."
-    ./test -- --repeat 100 || exit 1
-    cd ..
-fi
+echo "Stress testing on extension..."
+./test -- --repeat 100 || exit 1
+cd ..
+# fi
