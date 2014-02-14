c6c3126a3986ce9ed747cd72dd164c15542be005
Matt Way <io@mway.me>
2014-02-14 16:27:46 -0500

    support HTTP methods in action names

--- src/Pux/Controller.php
+++ src/Pux/Controller.php
@@ -1,44 +1,62 @@
 <?php
+
 namespace Pux;
 use ReflectionClass;
 use ReflectionObject;
 use ReflectionMethod;
 use Pux\Mux;
 
-class Controller {
 
+class Controller {
     public function getActionMethods() {
         $refObject = new ReflectionObject($this);
-        return array_map(function($m){ return $m->getName(); }, array_filter($refObject->getMethods(), function($m) { 
-            return preg_match('#Action$#', $m->getName() );
+        return array_map(function($m) { return $m->getName(); }, array_filter($refObject->getMethods(), function($m) { 
+            return preg_match('/Action$/', $m->getName());
         }));
     }
 
     public function getActionPaths() {
         $actionNames = array_map(function($method) {
-            return preg_replace('#Action$#','', $method);
-        }, $this->getActionMethods() );
+            return preg_replace('/Action$/', '', $method);
+        }, $this->getActionMethods());
 
         $pairs = array();
-        foreach( $actionNames as $actionName ) {
-            if ( $actionName == "index" ) {
+        foreach($actionNames as $actionName) {
+            $httpMethod = null;
+            $actionName = preg_replace_callback('/(Get|Put|Post|Delete)$/', function($matches) use(&$httpMethod) { 
+                $httpMethod = $matches[0];
+                return;
+            }, $actionName);
+
+            if($actionName == "index") {
                 $path = '';
             } else {
-                $path = '/' . preg_replace_callback('/[A-Z]/', function($regs) { 
-                    return '/' . strtolower($regs[0]);
+                $path = '/' . preg_replace_callback('/[A-Z]/', function($matches) { 
+                    return '/' . strtolower($matches[0]);
                 }, $actionName);
             }
-            $pairs[] = array($path, $actionName);
+
+            $pairs[] = array($path, $actionName . $httpMethod . 'Action', strtoupper($httpMethod));
         }
+
         return $pairs;
     }
 
     public function expand() {
-        $mux = new Mux;
+        $mux = new \Pux\Mux;
         $paths = $this->getActionPaths();
-        foreach( $paths as $path ) {
-            $mux->add($path[0], array(get_class($this), $path[1]) );
+
+        foreach($paths as $path) {
+            $opts = array();
+
+            if(isset($path[2])) {
+                $httpMethod = $mux->getRequestMethodConstant(strtoupper($path[2]));
+                $httpMethod && ($opts['method'] = $httpMethod);
+            }
+
+            $mux->add($path[0], array(get_class($this), $path[1]), $opts);
         }
+
         $mux->sort();
         return $mux;
     }
@@ -46,7 +64,4 @@ class Controller {
     public function toJson($data) {
         return json_encode($data);
     }
-
 }
-
-
