98aca2fd1b7797fabe4e18c91d739a728ce1492d
Matt Way <io@mway.me>
2014-02-17 10:36:20 -0500

    minor cleanup, ensure no URI slash prefix for consistency

--- src/Pux/Controller.php
+++ src/Pux/Controller.php
@@ -9,36 +9,39 @@ class Controller {
 
     public function getActionMethods() {
         $refObject = new ReflectionObject($this);
-        return array_map(function($m){ return $m->getName(); }, array_filter($refObject->getMethods(), function($m) { 
-            return preg_match('#Action$#', $m->getName() );
+        return array_map(function($m) { return $m->getName(); }, array_filter($refObject->getMethods(), function($m) { 
+            return preg_match('/Action$/', $m->getName());
         }));
     }
 
     public function getActionPaths() {
-        $actionNames = array_map(function($method) {
-            return preg_replace('#Action$#','', $method);
-        }, $this->getActionMethods() );
+        $pairs          = array();
+        $actionNames    = $this->getActionMethods();
 
-        $pairs = array();
-        foreach( $actionNames as $actionName ) {
-            if ( $actionName == "index" ) {
+        foreach ($actionNames as $actionName) {
+            $camelUrlAction = substr($actionName, 0, -6);
+            if ($camelUrlAction === 'index') {
                 $path = '';
             } else {
-                $path = '/' . preg_replace_callback('/[A-Z]/', function($regs) { 
+                $path = preg_replace_callback('/[A-Z]/', function($regs) { 
                     return '/' . strtolower($regs[0]);
-                }, $actionName);
+                }, $camelUrlAction);
             }
-            $pairs[] = array($path, $actionName);
+
+            $pairs[] = array(ltrim($path, '/'), $actionName);
         }
+
         return $pairs;
     }
 
     public function expand() {
-        $mux = new Mux;
-        $paths = $this->getActionPaths();
-        foreach( $paths as $path ) {
-            $mux->add($path[0], array(get_class($this), $path[1]) );
+        $mux    = new Mux();
+        $paths  = $this->getActionPaths();
+
+        foreach ($paths as $path) {
+            $mux->add($path[0], array(get_class($this), $path[1]));
         }
+
         $mux->sort();
         return $mux;
     }
