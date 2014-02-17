8481152f112cb5e050d701bc1dbcd40491ad4fb6
Matt Way <io@mway.me>
2014-02-17 16:38:25 -0500

    don't double slashes when concatenating paths

--- src/Pux/Mux.php
+++ src/Pux/Mux.php
@@ -79,7 +79,7 @@ class Mux
                 } else {
                     $this->routes[] = array(
                         false,
-                        $pattern . $route[1],
+                        rtrim($pattern, '/') . '/' . $route[1],
                         $route[2],
                         isset($route[3]) ? array_merge($options, $route[3]) : $options,
                     );
