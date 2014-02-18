bbf7a76fd47b5e95b4269bd194892db86697c654
Matt Way <io@mway.me>
2014-02-18 01:02:22 -0500

    move docblock parsing to Controller::expand()

--- src/Pux/Controller.php
+++ src/Pux/Controller.php
@@ -15,45 +15,45 @@ class Controller {
     }
 
     public function getActionPaths() {
-        $tuples         = array();
-        $actionNames    = $this->getActionMethods();
+        $pairs          = array();
+        $actionNames    = array_map(function($method) {
+            return preg_replace('/Action$/', '', $method);
+        }, $this->getActionMethods());
 
         foreach ($actionNames as $actionName) {
-            $rmth   = new ReflectionMethod($this, $actionName);
-            $doc    = $rmth->getDocComment();
-
-            if(!$doc) {
-                continue;
+            if ($actionName === 'index') {
+                $path = '';
+            } else {
+                $path = preg_replace_callback('/[A-Z]/', function($matches) {
+                    return '/' . strtolower($matches[0]);
+                }, $actionName);
             }
 
-            $camelUrlAction = substr($actionName, 0, -6);
-            $path = $method = null;
-
-            if(preg_match('/^[\s*]*\@method (get|put|post|delete|head|patch|options)\s*$/im', $doc, $mmatch)) {
-                $method = array_pop($mmatch);
-            }
-
-            if(preg_match('/^[\s*]*\@uri ([^\s]+)\s*$/im', $doc, $umatch)) {
-                $path = ltrim(array_pop($umatch), '/');
-            } else if($camelUrlAction !== 'index') {
-                $path = preg_replace_callback('/[A-Z]/', function($regs) { 
-                    return '/' . strtolower($regs[0]);
-                }, $camelUrlAction);
-            }
-
-            $tuples[] = array(ltrim($path, '/'), $actionName, $method);
+            $pairs[] = array(ltrim($path, '/'), $actionName . 'Action');
         }
 
-        return $tuples;
+        return $pairs;
     }
 
     public function expand() {
         $mux    = new Mux();
         $paths  = $this->getActionPaths();
-
+        
         foreach ($paths as $path) {
-            $opts = array();
-            isset($path[2]) && ($opts['method'] = $mux->getRequestMethodConstant($path[2]));
+            $rmth   = new ReflectionMethod($this, $path[1]);
+            $doc    = $rmth->getDocComment();
+            $opts   = array();
+
+            if($doc) {
+                if(preg_match('/^[\s*]*\@method (get|put|post|delete|head|patch|options)\s*$/im', $doc, $mmatch)) {
+                    $opts['method'] = $mux->getRequestMethodConstant(array_pop($mmatch));
+                }
+
+                if(preg_match('/^[\s*]*\@uri ([^\s]+)\s*$/im', $doc, $umatch)) {
+                    $path[0] = ltrim(array_pop($umatch), '/');
+                }
+            }
+
             $mux->add($path[0], array(get_class($this), $path[1]), $opts);
         }
 
