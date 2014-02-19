baefe643c73fe329666bd43d931b04918229fb0f
c9s <cornelius.howl@gmail.com>
2014-02-19 18:32:53 +0800

    Separate conditional tests to MuxConditionTest

---
+++ test/Pux/MuxConditionTest.php
@@ -0,0 +1,39 @@
+<?php
+use Pux\Mux;
+use Pux\Executor;
+
+class MuxConditionTest extends MuxTestCase
+{
+
+    public function testRouteWithDomainCondition() {
+        $mux = new \Pux\Mux;
+        $mux->expand = false;
+        ok($mux, "got mux");
+        $mux->add('/foo', [ 'HelloController2','indexAction' ], [ 'domain' => 'test.dev' ]);
+        $_SERVER['HTTP_HOST'] = 'test.dev';
+        $route = $mux->dispatch('/foo');
+        ok($route);
+
+        $_SERVER['HTTP_HOST'] = 'github.com';
+        $route = $mux->dispatch('/foo');
+        ok(! $route);
+    }
+
+    public function testMuxGetMethod() {
+        $mux = new \Pux\Mux;
+        ok($mux);
+        $mux->get('/news', [ 'NewsController','listAction' ]);
+        $mux->get('/news_item', [ 'NewsController','itemAction' ], []);
+
+        $routes = $mux->getRoutes();
+        ok($routes);
+        count_ok(2, $routes);
+        is( 2, $mux->length() );
+
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        ok( $mux->dispatch('/news_item') );
+        ok( $mux->dispatch('/news') );
+    }
+
+
+}
