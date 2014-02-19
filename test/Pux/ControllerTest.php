722da8744f62b59659021693acc0dc2c92a4a8fd
c9s <cornelius.howl@gmail.com>
2014-02-19 16:02:12 +0800

    make controller tests pass

--- test/Pux/ControllerTest.php
+++ test/Pux/ControllerTest.php
@@ -1,4 +1,24 @@
 <?php
+use Pux\Mux;
+use Pux\Executor;
+use Pux\Controller;
+
+class CRUDProductController extends Controller
+{
+    public function indexAction() 
+    {
+
+    }
+
+    public function addAction() {
+
+    }
+
+    public function delAction() {
+
+    }
+
+}
 
 class ControllerTest extends PHPUnit_Framework_TestCase
 {
@@ -35,5 +55,48 @@ class ControllerTest extends PHPUnit_Framework_TestCase
             ok( $mux->dispatch($path) , $path);
         }
     }
+
+    public function testControllerMethods() {
+        $controller = new CRUDProductController;
+        ok($controller);
+
+        $actions = $controller->getActionMethods();
+        ok($actions);
+        ok( is_array($actions), 'is array' );
+        count_ok( 3, $actions);
+
+        $paths = $controller->getActionRoutes();
+        return;
+        ok($paths);
+        count_ok(3, $paths);
+        ok( is_array($paths[0]) );
+        ok( is_array($paths[1]) );
+        ok( is_array($paths[2]) );
+
+        $mux = $controller->expand();
+        ok($mux);
+
+        ok( $routes = $mux->getRoutes() );
+        count_ok( 3, $routes );
+
+        ok( $controller->toJson(array('foo' => 1) ) );
+
+        $mainMux = new Mux;
+        $mainMux->mount( '/product' , $controller->expand() );
+        ok( $mainMux->getRoutes() ); 
+        ok( $mainMux->dispatch('/product') );
+        ok( $mainMux->dispatch('/product/add') );
+        ok( $mainMux->dispatch('/product/del') );
+
+        $mainMux = new Mux;
+        $mainMux->expand = false;
+        $mainMux->mount( '/product' , $controller->expand() );
+        ok( $mainMux->getRoutes() ); 
+        ok( $r = $mainMux->dispatch('/product') );
+        ok( $r = $mainMux->dispatch('/product/add') );
+        ok( $r = $mainMux->dispatch('/product/del') );
+    }
+
+
 }
 
