06d5d6430f4a33946d358faa7c3f7a3ac44fa50a
c9s <yoanlin93@gmail.com>
2014-05-26 15:30:51 +0800

    Add RESTfulControllerTest

---
+++ test/Pux/RESTfulControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+use Pux\Mux;
+use Pux\Executor;
+use Pux\Controller;
+
+class ProductResourceController extends Controller { 
+
+
+    /**
+     * @Method("POST")
+     * @Route("");
+     */
+    public function createAction() {
+
+    }
+
+    /**
+     * @Route("/:id")
+     * @Method("POST")
+     */
+    public function updateAction() {
+
+    }
+
+    /**
+     * @Route("/:id")
+     * @Method("GET")
+     */
+    public function getAction() {
+
+    }
+
+    /**
+     * @Route("/:id");
+     * @Method("DELETE")
+     */
+    public function deleteAction() {
+
+    }
+
+}
+
+class RESTfulControllerTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $con = new ProductResourceController;
+        ok($con);
+        $routes = $con->getActionRoutes();
+        ok($routes);
+
+        $methods = $con->getActionMethods();
+        ok($methods);
+
+        $productMux = $con->expand();  // there is a sorting bug (fixed), this tests it.
+        ok($productMux);
+
+        $root = new Mux;
+        ok($root);
+        $root->mount('/product', $con->expand() );
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        ok( $root->dispatch('/product/10') );
+
+        $_SERVER['REQUEST_METHOD'] = 'DELETE';
+        ok( $root->dispatch('/product/10') );
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        ok( $root->dispatch('/product') ); // create
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        ok( $root->dispatch('/product/10') ); // update
+    }
+}
+
