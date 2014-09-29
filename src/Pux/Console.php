7776cfdd7e4f268fb5aaa781aee406b6d6486f8e
c9s <yoanlin93@gmail.com>
2014-09-29 23:36:44 +0800

    Checking in changes prior to tagging of version 1.5.2.

--- src/Pux/Console.php
+++ src/Pux/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const NAME = 'phpux';
-    const VERSION = "1.5.1";
+    const VERSION = "1.5.2";
 
     public function init()
     {
