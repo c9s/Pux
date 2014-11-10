2faf391881288899ae3ef025acbdd5a0d419646b
Ruben de Vries <ruben@rubensayshi.com>
2014-11-10 10:56:01 +0100

    added failing test for options not being merged correctly

--- test/Pux/MuxMountTest.php
+++ test/Pux/MuxMountTest.php
@@ -121,14 +121,51 @@ class MuxMountTest extends MuxTestCase
         is(0, $mux->length());
 
         $submux = new \Pux\Mux;
-        $submux->any('/hello/there', array( 'HelloController2','indexAction' ));
+        $submux->any('/hello/there', array('HelloController2', 'indexAction'));
         ok($submux);
         ok($routes = $submux->getRoutes());
         is(1, $submux->length());
         is(0, $mux->length());
-        $mux->mount( '/sub/:country' , $submux);
+        $mux->mount('/sub/:country', $submux);
         $r = $mux->dispatch('/sub/UK/hello/there');
         ok($r);
         $this->assertPcreRoute($r, '/sub/:country/hello/there');
+    }
+
+    public function testSubmuxMergeOptions() {
+        $mux = new \Pux\Mux;
+        ok($mux);
+        is(0, $mux->length());
+
+        $submux = new \Pux\Mux;
+        $submux->any('/foo', array( 'HelloController2', 'indexAction' ), array(
+            'default' => array('suffix' => 'csv'),
+        ));
+        $submux->any('/hello/:name', array( 'HelloController2', 'indexAction' ), array(
+            'default' => array('suffix' => 'json')
+        ));
+        ok($submux);
+        ok($routes = $submux->getRoutes());
+        is(2, $submux->length());
+        is(0, $mux->length());
+        $mux->mount( '/sub', $submux, array(
+            'default' => array('suffix' => 'xml', 'prefix' => 'v1'),
+            'require' => array('suffix' => '[a-z]{3,4}')
+        ));
+        is(2, $mux->length());
+
+        $r = $mux->dispatch('/sub/hello/John');
+        ok($r);
+        ok($r[3]['default']['suffix'] == 'json');
+        ok($r[3]['default']['prefix'] == 'v1');
+        ok($r[3]['require']['suffix'] == '[a-z]{3,4}');
+        $this->assertPcreRoute($r, '/sub/hello/:name');
+
+        $r = $mux->dispatch('/sub/foo');
+        ok($r);
+        ok($r[3]['default']['suffix'] == 'csv');
+        ok($r[3]['default']['prefix'] == 'v1');
+        ok($r[3]['require']['suffix'] == '[a-z]{3,4}');
+        $this->assertNonPcreRoute($r, '/sub/foo');
     }
 }
