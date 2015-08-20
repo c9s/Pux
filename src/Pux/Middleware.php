9e93b7e37c697b48fecfa39f915093828f7cc143
c9s <yoanlin93@gmail.com>
2015-08-20 18:25:21 +0800

    Refine middleware class

--- src/Pux/Middleware.php
+++ src/Pux/Middleware.php
@@ -6,30 +6,35 @@ class Middleware
     /**
      * @var Middleware
      */
-    public $next;
+    protected $next;
+
+
+
 
     public function __construct(callable $next)
     {
         $this->next = $next;
     }
 
-    public function call($env, $res)
-    {
-        try {
-            if ($n = $this->next) {
-                $res = $n($env, $res);
-            }
-            // $res = $next($env, $res);
-        } catch (Exception $e) {
 
-        }
-        return $res;
+    /**
+     * The wrapping logic of the middleware
+     *
+     * @param array $environment
+     * @param array $response
+     * @return array Response array
+     */
+    public function call(array $environment, array $response)
+    {
+        return $response;
     }
 
-    public function __invoke($env, $res)
+    public function __invoke(array $environment, array $response)
     {
-        $n = $this->next;
-        return $n($env, $res);
+        if ($n = $this->next) {
+            return $n($environment, $response);
+        }
+        return $response;
     }
     
 }
