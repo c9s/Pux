d6297d0b9f6e54dac9c516b25a0db3b5b79760a0
c9s <yoanlin93@gmail.com>
2015-08-22 20:20:34 +0800

    Fix Executor coding style

--- src/Pux/Executor.php
+++ src/Pux/Executor.php
@@ -1,17 +1,26 @@
 <?php
+
 namespace Pux;
+
 use Exception;
 use LogicException;
 use ReflectionClass;
-use ReflectionObject;
-use ReflectionParameter;
 
 class Executor
 {
     /**
-     * Execute the matched route
+     * Execute the matched route.
      *
      * $route: {pcre flag}, {pattern}, {callback}, {options}
+     *
+     * *callback*:
+     *
+     * The callback argument can be an array that contains a class name and a method name to be called.
+     * a closure object or a function name.
+     *
+     * *options*:
+     *
+     * 'constructor_args': arguments for constructing controller object.
      *
      * @return string the response
      */
@@ -20,7 +29,7 @@ class Executor
         list($pcre, $pattern, $cb, $options) = $route;
 
         // create the reflection class
-        $rc = new ReflectionClass( $cb[0] );
+        $rc = new ReflectionClass($cb[0]);
 
         $constructArgs = null;
         if (isset($options['constructor_args'])) {
@@ -36,7 +45,7 @@ class Executor
         }
 
         // check controller action method
-        if ($controller && ! method_exists( $controller ,$cb[1])) {
+        if ($controller && !method_exists($controller, $cb[1])) {
             throw new LogicException("Controller action method '{$cb[1]}' doesn't exist.");
             /*
             throw new Exception('Method ' .
@@ -45,9 +54,6 @@ class Executor
         }
 
         $rps = $rc->getMethod($cb[1])->getParameters();
-
-        // XXX:
-
         $vars = isset($options['vars'])
                 ? $options['vars']
                 : array()
@@ -56,20 +62,16 @@ class Executor
         $arguments = array();
         foreach ($rps as $param) {
             $n = $param->getName();
-            if (isset( $vars[ $n ] ))
-            {
+            if (isset($vars[ $n ])) {
                 $arguments[] = $vars[ $n ];
-            }
-            else if (isset($route[3]['default'][ $n ] )
-                            && $default = $route[3]['default'][ $n ] )
-            {
+            } elseif (isset($route[3]['default'][ $n ])
+                            && $default = $route[3]['default'][ $n ]) {
                 $arguments[] = $default;
-            }
-            else if ( ! $param->isOptional() && ! $param->allowsNull() ) {
+            } elseif (!$param->isOptional() && !$param->allowsNull()) {
                 throw new Exception('parameter is not defined.');
             }
         }
+
         return call_user_func_array($cb, $arguments);
     }
 }
-
