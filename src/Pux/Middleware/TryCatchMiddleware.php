c378c0941362b3bf32327bb97a1845a2d2b83d89
c9s <yoanlin93@gmail.com>
2015-08-22 21:00:48 +0800

    Add simple TryCatchMiddleware for experiments

---
+++ src/Pux/Middleware/TryCatchMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+namespace Pux\Middleware;
+use Pux\Middleware;
+
+class TryCatchMiddleware extends Middleware
+{
+    protected $options = array();
+
+    public function __construct(callable $app, array $options = array()) {
+        parent::__construct($app);
+        $this->options = $options;
+    }
+
+
+    public function call(array $environment, array $response)
+    {
+        try {
+            return parent::call($environment, $response);
+        } catch (Exception $e) {
+            if (isset($this->options['throw'])) {
+                throw $e;
+            }
+        }
+        return $response;
+    }
+}
+
