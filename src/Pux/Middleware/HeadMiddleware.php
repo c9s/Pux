d77b4a72a4f2a513844d8468a4b316094fc46f8c
c9s <yoanlin93@gmail.com>
2015-08-23 18:59:48 +0800

    HeadMiddleware: Remove the response body

--- src/Pux/Middleware/HeadMiddleware.php
+++ src/Pux/Middleware/HeadMiddleware.php
@@ -4,6 +4,13 @@ use Pux\Middleware;
 use Negotiation\FormatNegotiator;
 use Negotiation\Negotiator;
 
+/**
+ * HeadMiddleware: Auto delete response body in HEAD requests.
+ *
+ * This middleware deletes response body in HEAD requests.
+ * 
+ * Middleware Port of https://metacpan.org/pod/Plack::Middleware::Head
+ */
 class HeadMiddleware extends Middleware
 {
     public function call(array & $environment, array $response)
@@ -12,6 +19,10 @@ class HeadMiddleware extends Middleware
             // FIXME: return callback to the app so that we can remove the
             // response body
             $response = parent::call($environment, $response);
+
+            // Clean up the response body
+            $response[2] = '';
+            return $response;
         }
         return parent::call($environment, $response);
     }
