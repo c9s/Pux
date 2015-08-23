e3f6eaa4e8ec4d7eaf811dd20056caedabb1abc2
c9s <yoanlin93@gmail.com>
2015-08-23 12:19:33 +0800

    Dispatch rest path_info and save the original path info in $env
    Fixes: #92

--- src/Pux/App/MuxApp.php
+++ src/Pux/App/MuxApp.php
@@ -27,8 +27,20 @@ class MuxApp
     public function call(array & $environment, array $response)
     {
         if ($route = $this->mux->dispatch($environment['PATH_INFO'])) {
+            $path = $route[1];
             $app = $route[2];
+
+            // Save the original PATH_INFO in ORIG_PATH_INFO
+            // Note that some SAPI implementation will save 
+            // use the ORIG_PATH_INFO (not noticed yet)
+            if (!isset($environment['ORIG_PATH_INFO'])) {
+                $environment['ORIG_PATH_INFO'] = $environment['PATH_INFO'];
+            }
+            $environment['PATH_INFO'] = substr($environment['PATH_INFO'], strlen($path));
+
             if ($app instanceof Middleware) { 
+
+
                 return $app($environment, $response);
             } else {
                 return Executor::execute($route);
