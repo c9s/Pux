04847fc2b9d80d11135b395a10c7d39f97af3076
c9s <yoanlin93@gmail.com>
2015-08-29 13:44:20 +0800

    Improve RouteExecutor

--- src/Pux/RouteExecutor.php
+++ src/Pux/RouteExecutor.php
@@ -5,9 +5,25 @@ namespace Pux;
 use Exception;
 use LogicException;
 use ReflectionClass;
+use Closure;
 
 class RouteExecutor
 {
+
+    /**
+     * @var boolean 
+     *
+     * Enable executor to use reflection methods to validate controller action
+     *              parameter prototype.
+     */
+    protected $validateControllerAction = false;
+
+    public function __construct()
+    {
+
+    }
+
+
     /**
      * Execute the matched route.
      *
@@ -24,24 +40,43 @@ class RouteExecutor
      *
      * @return string the response
      */
-    public static function execute(array $route)
+    public static function execute(array $route, array $environment = array())
     {
         list($pcre, $pattern, $cb, $options) = $route;
+
+        if ($cb instanceof Closure) {
+            return $cb($environment);
+        }
+
+
+        // Start handing controller dispatch from here
 
         // create the reflection class
         $rc = new ReflectionClass($cb[0]);
 
+        // If users define the constructor arguments in options array.
         $constructArgs = null;
         if (isset($options['constructor_args'])) {
             $constructArgs = $options['constructor_args'];
         }
 
-        // if the first argument is a class name string,
+        // If the first argument is a class name string,
         // then create the controller object.
         if (is_string($cb[0])) {
-            $cb[0] = $controller = $constructArgs ? $rc->newInstanceArgs($constructArgs) : $rc->newInstance();
-        } else {
+            if (is_a($cb[0],'Pux\\Controller')) {
+
+                $cb[0] = $controller = $rc->newInstanceArgs([$environment]);
+
+            } else {
+                $cb[0] = $controller = $constructArgs 
+                    ? $rc->newInstanceArgs($constructArgs) 
+                    : $rc->newInstance();
+            }
+        } else if (is_object($cb[0])) {
+            // If it's a dynamic controller object
             $controller = $cb[0];
+        } else {
+            // unsupported callable type
         }
 
         // check controller action method
@@ -50,24 +85,24 @@ class RouteExecutor
             /*
             throw new Exception('Method ' .
                 get_class($controller) . "->{$cb[1]} does not exist.", $route );
-             */
+            */
         }
 
-        $rps = $rc->getMethod($cb[1])->getParameters();
+        $reflParameters = $rc->getMethod($cb[1])->getParameters();
         $vars = isset($options['vars'])
                 ? $options['vars']
                 : array()
                 ;
 
         $arguments = array();
-        foreach ($rps as $param) {
-            $n = $param->getName();
+        foreach ($reflParameters as $reflParameter) {
+            $n = $reflParameter->getName();
             if (isset($vars[ $n ])) {
                 $arguments[] = $vars[ $n ];
             } elseif (isset($route[3]['default'][ $n ])
                             && $default = $route[3]['default'][ $n ]) {
                 $arguments[] = $default;
-            } elseif (!$param->isOptional() && !$param->allowsNull()) {
+            } elseif (!$reflParameter->isOptional() && !$reflParameter->allowsNull()) {
                 throw new Exception('parameter is not defined.');
             }
         }
